bc2fd04bd8b1b0c609ef4b1e5c89af316dbc81f9
Diego Almirón <diego90dionisio@gmail.com>
2021-08-14 00:45:01 -0300

    Dibujo un cuadrado en el canvas

--- src/board.js
+++ src/board.js
@@ -1,3 +1,9 @@
-!(function(){
+
+!(function () {
     console.log("Estructura principal del nuetro magic board");
+    const canvas = document.getElementById("pizarra");
+    const board = canvas.getContext("2d");
+
+    board.fillStyle = 'green';
+    board.fillRect(10, 10, 150, 100);
 })();
